5e05cb73cbc7c7e1aaa09265ab050e62298e4fb5
Jeffsan <jeffsan.wang@gmail.com>
2016-04-28 16:36:14 +0800

    add acm devices

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo "Author Jeffsan Chen Wang: jeffsan.wang@gmail.com"
 sudo cp git-prompt.sh ~/
 sudo cp git-completion.bash ~/
 sudo cp myros.sh ~/
-sudo cp 52-ftdi.rules /etc/udev/rules.d
+sudo cp rules/*  /etc/udev/rules.d/
 sudo cat ./.bashrc >> ~/.bashrc
 echo "done."
